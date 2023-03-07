5b6fe17312bd9db7636f1d1c9f5bd47d694b3329
Yichen Li <yichen.li.1@stonybrook.edu>
2023-03-06 21:25:05 -0500

    21:24, part1 complete

--- src/wolfpack.c
+++ src/wolfpack.c
@@ -20,13 +20,58 @@
 #include "wolfpack.h"
 
 
-void get_source_address(const unsigned char *packet){
-    unsigned char* p = packet;
-    printf("%#02x\n", p);
+// General print function to print data fields, e.g. source address, destination address...
+void print_bytes(const unsigned char *packet, int start, int length){
+    unsigned char* p = (unsigned char*)packet + start;
+    for(int i = 0; i < length; i++){
+        printf("%02x", *p);
+        p++;
+    }
+    printf("\n");
     
 }
 
+
+int get_total_length(const unsigned char *packet){
+    int result = 0;
+    unsigned char* p = (unsigned char*)packet + 17;
+    for(int i = 0; i < 3; i++){
+        result = result << 8;
+        result = result + *p;
+        p++;
+    }
+    return result;
+}
+
+// Print function to print the payloads. 
+void print_to_end(const unsigned char *packet, int start){
+    unsigned char* p = (unsigned char*)packet + start;
+   
+    // printf("%d\n", get_total_length(packet));
+
+    int end = get_total_length(packet) - 24;
+    for(int i = 0; i < end; i++){
+        printf("%c", *p);
+        p++;
+    }
+
+
+    printf("\n");
+}
+
+// Print function. 
 void print_packet_sf(const unsigned char *packet) {
+    unsigned char* p = (unsigned char*)packet;
+    print_bytes(p, 0, 5);
+    print_bytes(p, 5, 5);
+    print_bytes(p, 10, 1);
+    print_bytes(p, 11, 1);
+    print_bytes(p, 12, 3);
+    print_bytes(p, 15, 2);
+    print_bytes(p, 17, 3);
+    print_bytes(p, 20, 4);
+    print_to_end(p, 24);
+    // printf("\n");
 }
 
 unsigned int packetize_sf(const char *message, unsigned char *packets[], unsigned int packets_len, unsigned int max_payload,
